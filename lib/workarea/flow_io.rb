346e5e9f53e664110b9c12d38dae91dca7fdbc63
Eric Pigeon <epigeon@weblinc.com>
2018-06-14 14:03:09 -0400

    Add creditional and config accessors
    no changelog

--- lib/workarea/flow_io.rb
+++ lib/workarea/flow_io.rb
@@ -7,5 +7,20 @@ require 'workarea/flow_io/version'
 
 module Workarea
   module FlowIo
+    def self.credentials
+      (Rails.application.secrets.flow_io || {}).deep_symbolize_keys
+    end
+
+    def self.config
+      Workarea.config.flow_io
+    end
+
+    def self.api_token
+      credentials[:api_token]
+    end
+
+    def self.organization_id
+      credentials[:organization_id]
+    end
   end
 end
