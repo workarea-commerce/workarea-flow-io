a9b98a1d493ca9b71160f2e13329ced81535e05c
Jeff Yucis <jyucis@weblinc.com>
2018-08-28 11:59:37 -0400

    Display Flow prices in Admin order screen
    Improves the UI so admins can see base and experience pricing
    for orders.
    
    no changelog
    
    FLOW-33

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Workarea::Storefront::Engine.routes.draw do
   post :flow_webhook, as: :flow_io_webhook, controller: 'flow_io_webhook', action: 'event'
 end
+
+Workarea::Admin::Engine.routes.draw do
+  resources :orders, only: [] do
+    member do
+      get :flow
+    end
+  end
+end
--- test/integration/workarea/admin/flow_order_integration_test.rb
+++ test/integration/workarea/admin/flow_order_integration_test.rb
@@ -6,7 +6,7 @@ module Workarea
       include Admin::IntegrationTest
       include Workarea::FlowIo::FlowFixtures
 
-      def test_returns_flow_order_details
+      def test_returns_flow_order_attributes
         order = create_placed_canadian_flow_order
 
         get admin.order_path(order)
@@ -15,6 +15,26 @@ module Workarea
         assert(response.body.include?("ord-7d170417473c43c99d7ea88f938bfebb"))
 
         assert(response.body.include?(I18n.t('workarea.admin.orders.view_flow_order')))
+      end
+
+      def test_returns_flow_order_details
+        order = create_placed_canadian_flow_order
+
+        get admin.flow_order_path(order)
+
+        assert(response.body.include?("ord-7d170417473c43c99d7ea88f938bfebb"))
+
+        assert(response.body.include?(order.total_price.to_s))
+        assert(response.body.include?(order.flow_total_price.to_s))
+
+        assert(response.body.include?(order.tax_total.to_s))
+        assert(response.body.include?(order.flow_tax_total.to_s))
+
+        assert(response.body.include?(order.shipping_total.to_s))
+        assert(response.body.include?(order.flow_shipping_total.to_s))
+
+        assert(response.body.include?(order.items.first.total_price.to_s))
+        assert(response.body.include?(order.items.first.flow_total_price.to_s))
       end
     end
   end
