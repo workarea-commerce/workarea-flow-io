b230529f486512284e4adbbf07de1a6e48f83012
Tom Scott <tscott@weblinc.com>
2020-03-10 11:56:48 -0400

    Remove to_flow_label since it is no longer used

--- app/models/workarea/flow_io/local_item.rb
+++ app/models/workarea/flow_io/local_item.rb
@@ -33,19 +33,6 @@ module Workarea
           quantity >= localized_price.min_quantity
         end&.to_price || Workarea::Pricing::Price.new(regular: 0.to_m(experience.currency))
       end
-
-      # TODO determine if we still need the original
-      #
-      #
-      def to_flow_label(quantity = 1, &block)
-        price = prices.sort_by(&:min_quantity).reverse.detect do |localized_price|
-          quantity >= localized_price.min_quantity
-        end
-
-        return if price.blank?
-
-        block.call(price)&.label
-      end
     end
   end
 end
